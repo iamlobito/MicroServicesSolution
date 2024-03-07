98cc4164a51aad6fa4ca0d5fafcb28141e57247f
Nelson Silva <e1192@itsector.pt>
2024-03-07 16:23:36 +0000

    Add Api Gateway with Ocelot

---
+++ Demo.ApiGateway/Program.cs
@@ -0,0 +1,30 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.UseOcelot();
+
+app.Run();
